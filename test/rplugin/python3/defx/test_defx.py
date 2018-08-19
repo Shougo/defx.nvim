2c351e66bc59d3699a66f82f2f69cf6e48e497f6
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-08-20 07:43:47 +0900

    Fix test error

--- test/rplugin/python3/defx/test_defx.py
+++ test/rplugin/python3/defx/test_defx.py
@@ -6,7 +6,7 @@ from unittest.mock import create_autospec
 from unittest.mock import MagicMock
 
 
-def test_do_action():
+def test_view():
     vim = create_autospec(Nvim)
     vim.channel_id = 0
     vim.vars = {}
@@ -14,5 +14,3 @@ def test_do_action():
     vim.current = MagicMock()
 
     defx = View(vim, [], {})
-    defx.redraw()
-    defx.do_action('open', [], {})
