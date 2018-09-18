a74c54c6d6f4b526591e41adb90dd1421be231aa
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-09-18 23:17:52 +0900

    Remove kind

--- rplugin/python3/defx/source/file.py
+++ rplugin/python3/defx/source/file.py
@@ -19,14 +19,12 @@ class Source(Base):
         super().__init__(vim)
 
         self.name = 'file'
-        self.kind = 'file'
 
     def get_root_candidate(self, context: Context, path: str) -> dict:
         return {
             'word': path,
             'abbr': self.vim.call('fnamemodify',
                                   path + ('/' if path != '/' else ''), ':~'),
-            'kind': 'directory',
             'is_directory': True,
             'action__path': path,
         }
@@ -41,7 +39,6 @@ class Source(Base):
             candidates.append({
                 'word': str(entry),
                 'abbr': entry.name + ('/' if entry.is_dir() else ''),
-                'kind': ('directory' if entry.is_dir() else 'file'),
                 'is_directory': entry.is_dir(),
                 'action__path': str(entry),
             })
