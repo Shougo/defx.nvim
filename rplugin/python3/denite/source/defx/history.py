f5f577fbe3fe930e0b92eafd8ee792633943110e
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-11-06 00:04:09 +0900

    Improve defx/history source

--- rplugin/python3/denite/source/defx/history.py
+++ rplugin/python3/denite/source/defx/history.py
@@ -15,9 +15,17 @@ class Source(Base):
 
         self.name = 'defx/history'
         self.kind = 'command'
+        self._histories = []
+
+    def on_init(self, context: dict):
+        if self.vim.current.buffer.options.get('filetype', '') != 'defx':
+            return
+
+        self._histories = reversed(self.vim.vars['defx#_histories'])
 
     def gather_candidates(self, context: dict):
         return [{
             'word': x,
+            'abbr': x + '/',
             'action__command': f"call defx#_do_action('cd', ['{x}'])"
-        } for x in reversed(self.vim.vars['defx#_histories'])]
+        } for x in self._histories]
