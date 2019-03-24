106014049d043693f9d0c7288488689f5d4232af
Shougo Matsushita <Shougo.Matsu@gmail.com>
2019-03-24 17:34:47 +0900

    Use resolve() in cd

--- rplugin/python3/defx/kind/file.py
+++ rplugin/python3/defx/kind/file.py
@@ -87,7 +87,7 @@ def _cd(view: View, defx: Defx, context: Context) -> None:
     Change the current directory.
     """
     path = Path(context.args[0]) if context.args else Path.home()
-    path = Path(defx._cwd).joinpath(path)
+    path = Path(defx._cwd).joinpath(path).resolve()
     if not readable(path) or not path.is_dir():
         error(view._vim, f'{path} is not readable directory')
         return
