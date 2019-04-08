0dce10bb861d8a0e657af23c493d154eee891ca7
HP-manjaro <zgpio@qq.com>
2019-04-08 16:05:16 +0800

    Abstract indent to column

--- rplugin/python3/defx/column/filename.py
+++ rplugin/python3/defx/column/filename.py
@@ -18,24 +18,17 @@ class Column(Base):
 
         self.name = 'filename'
         self.vars = {
-            'directory_icon': '+',
-            'indent': ' ',
             'min_width': 40,
             'max_width': 100,
-            'opened_icon': '-',
-            'root_icon': ' ',
             'root_marker_highlight': 'Constant',
         }
 
         self._current_length = 0
         self._syntaxes = [
             'directory',
-            'directory_icon',
             'hidden',
             'marker',
-            'opened_icon',
             'root',
-            'root_icon',
         ]
         self._context: Context = Context()
 
@@ -44,17 +37,7 @@ class Column(Base):
 
     def get(self, context: Context,
             candidate: typing.Dict[str, typing.Any]) -> str:
-        if candidate['is_opened_tree']:
-            icon = self.vars['opened_icon']
-        elif candidate['is_root']:
-            icon = self.vars['root_icon']
-        elif candidate['is_directory']:
-            icon = self.vars['directory_icon']
-        else:
-            icon = ' '
-        return self._truncate(
-            self.vars['indent'] * candidate['level'] +
-            icon + ' ' + candidate['word'])
+        return self._truncate(candidate['word'])
 
     def length(self, context: Context) -> int:
         max_fnamewidth = max([self._strwidth(x['word'])
@@ -69,19 +52,6 @@ class Column(Base):
 
     def highlight_commands(self) -> typing.List[str]:
         commands: typing.List[str] = []
-        for icon, highlight in {
-                'directory': 'Special',
-                'opened': 'Special',
-                'root': 'Identifier',
-        }.items():
-            commands.append(
-                ('syntax match {0}_{1}_icon /[{2}]/ ' +
-                 'contained containedin={0}_directory').format(
-                    self.syntax_name, icon, self.vars[icon + '_icon']))
-            commands.append(
-                'highlight default link {}_{}_icon {}'.format(
-                    self.syntax_name, icon, highlight))
-
         commands.append(
             r'syntax match {0}_{1} /\S.*[\\\/]/ '
             'contained containedin={0}'.format(
