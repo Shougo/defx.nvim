5d7c3e11dcb36eec26ce9f229cf121ea5aa0c64e
matsui54 <haru.matu9168@gmail.com>
2021-04-25 19:07:36 +0900

    Allow external sources to copy files to local

--- rplugin/python3/defx/clipboard.py
+++ rplugin/python3/defx/clipboard.py
@@ -14,10 +14,19 @@ class ClipboardAction(Enum):
     LINK = auto()
 
 
+def default_paster(src: str, dest: str) -> None:
+    pass
+
+
 class Clipboard():
     def __init__(self,
                  action: ClipboardAction = ClipboardAction.COPY,
                  candidates:
-                 typing.List[typing.Dict[str, typing.Any]] = []) -> None:
+                 typing.List[typing.Dict[str, typing.Any]] = [],
+                 source_name: str = 'file',
+                 paster: typing.Callable[[str, str], None] = default_paster
+                 ) -> None:
         self.action = action
         self.candidates = candidates
+        self.source_name = source_name
+        self.paster = paster
